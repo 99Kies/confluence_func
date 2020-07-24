e13ede0de6ff87034f5fdb7188b155d3a5c305be
99kies <1290017556@qq.com>
2020-07-24 13:42:51 +0800

    update

--- confluence_func.py
+++ confluence_func.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: gb2312 -*-
+# -*- coding: utf-8 -*-
 
 from atlassian import Confluence
 from pprint import pprint
 import time
 
 class ConfluenceService():
-    def __init__(self, URL, USERNAME, PASSWD, source_space, target_space=None):
+    def __init__(self, URL, USERNAME, PASSWD):
         """
          Login in
         :param URL: input service website
@@ -15,8 +15,6 @@ class ConfluenceService():
         :type USERNAME: str
         :param PASSWD: input password
         :type PASSWD: str
-        :param source_space: the space that Page is in
-        :type source_space: str
         :return: None
         """
         try:
@@ -28,30 +26,24 @@ class ConfluenceService():
         except:
             print('login failed!')
             return False
-        self.source_space = source_space
-        self.msg = self.get_body(self.source_space)
-        self.wrong_title = []
-        if target_space:
-            self.target_space = target_space
-            self.create_body(self.msg, self.target_space)
 
-    def get_body(self, source_space):
+    def get_body(self, SPACE):
         """
         Gets the content of the article in the space
-
+        :param SPACE: the space that Page is in
+        :type SPACE: str
         :return: msg_body as dict
         """
-        all_page = self.confluence.get_all_pages_from_space(source_space, start=0, status=None, expand=None,limit=30,
+        self.source_space = SPACE
+        all_page = self.confluence.get_all_pages_from_space(SPACE, start=0, status=None, expand=None,
                                                             content_type='page')
 
         msg_body = {}
         child_parent = {}
-        n = 0
         for i in all_page:
-            n = 1+n
             child = self.confluence.get_page_child_by_type(i['id'],type='page', start=None, limit=None)
 
-            body = self.confluence.get_page_by_id(i['id'], "body.view", status=None ,version=None)
+            body = self.confluence.get_page_by_id(i['id'], "body.view", status=None, version=None)
             msg_body[body['id']] = {
 
             }
@@ -69,7 +61,6 @@ class ConfluenceService():
                         print(msg_body[c]['title'] ,'\t\t\t\t\t\t\t\t', msg_body[c]['parent_title'])
                     except:
                         pass
-        print('num: ',  n)
         return msg_body
 
     def create_body(self, msg_body, target_space):
@@ -87,71 +78,43 @@ class ConfluenceService():
 
         for _id, msg in msg_body.items():
             if msg.get('title') == self.source_space or msg.get('parent_title')==None:
-                try:
-                    self.confluence.create_page(
-                        space=target_space,
-                        title=msg['title'],
-                        body=msg['body'],
-                        type=msg['type']
-                    )
-                    new_id = self.confluence.get_page_id(target_space, msg['title'])
-                    new_parent_msg[msg['title']] = new_id
-                    # break
-                except Exception as e:
+
+                for i in range(10):
                     try:
-                        time.sleep(3)
-                        self.confluence.create_page(
+                        status = self.confluence.create_page(
                             space=target_space,
                             title=msg['title'],
-                            body='wrong',
-                            type=msg['type']
+                            body=msg['body'],
                         )
+                        time.sleep(1)
                         new_id = self.confluence.get_page_id(target_space, msg['title'])
                         new_parent_msg[msg['title']] = new_id
-                        self.wrong_title.append(msg['title'])
+                        break
+                    except Exception as e:
+                        time.sleep(3)
                         print(e.args)
-                        # print(msg)
-                    except:
-                        pass
+
             else:
 
                 print(msg['title'], msg.get('parent_title'), new_parent_msg.get(msg.get('parent_title')))
-                try:
-                    self.confluence.create_page(
-                        space=target_space,
-                        title=msg['title'],
-                        body=msg['body'],
-                        parent_id=new_parent_msg.get(msg.get('parent_title')),
-                        type=msg['type']
-                    )
-                    new_id = self.confluence.get_page_id(target_space, msg['title'])
-                    new_parent_msg[msg['title']] = new_id
-                    # break
-                except Exception as e:
+                for i in range(10):
                     try:
-                        time.sleep(3)
-                        self.confluence.create_page(
+                        status = self.confluence.create_page(
                             space=target_space,
                             title=msg['title'],
-                            body='wrong',
-                            parent_id=new_parent_msg.get(msg.get('parent_title')),
-                            type=msg['type']
+                            body=msg['body'],
+                            parent_id=new_parent_msg.get(msg.get('parent_title'))
                         )
+                        time.sleep(1)
                         new_id = self.confluence.get_page_id(target_space, msg['title'])
                         new_parent_msg[msg['title']] = new_id
+                        break
+                    except Exception as e:
+                        time.sleep(3)
                         print(e.args)
-                        # print(msg)
-                    except:
-                        pass
 
 
-        print()
-        print()
-        print()
-        print(self.wrong_title)
-
-
-def create_body_other_website(URL, USERNAME, PASSWD, msg_body, source_space, target_space):
+def create_body_other_website(URL, USERNAME, PASSWD, msg_body, target_space, source_space):
     """
      Create pages on another service
     :param URL: input service website
@@ -182,66 +145,54 @@ def create_body_other_website(URL, USERNAME, PASSWD, msg_body, source_space, tar
     }
 
     for _id, msg in msg_body.items():
+        # print(msg['title'],'\t',msg.get('parent_title'))
         if msg.get('title') == source_space or msg.get('parent_title')==None:
 
-            try:
-                confluence.create_page(
-                    space=target_space,
-                    title=msg['title'],
-                    body=msg['body'],
-                    type=msg['type']
-                )
-                time.sleep(1)
-                new_id = confluence.get_page_id(target_space, msg['title'])
-                new_parent_msg[msg['title']] = new_id
-                # break
-            except Exception as e:
-                time.sleep(3)
-                confluence.create_page(
-                    space=target_space,
-                    title=msg['title'],
-                    body='wrong',
-                    type=msg['type']
-                )
-                print(e.args)
-            
+            for i in range(10):
+                try:
+                    status = confluence.create_page(
+                        space=target_space,
+                        title=msg['title'],
+                        body=msg['body'],
+                        # type=msg['type']
+                    )
+                    time.sleep(1)
+                    new_id = confluence.get_page_id(target_space, msg['title'])
+                    new_parent_msg[msg['title']] = new_id
+                    break
+                except Exception as e:
+                    time.sleep(3)
+                    print(e.args)
+
         else:
 
             print(msg['title'], msg.get('parent_title'), new_parent_msg.get(msg.get('parent_title')))
-
-            try:
-                confluence.create_page(
-                    space=target_space,
-                    title=msg['title'],
-                    body=msg['body'],
-                    parent_id=new_parent_msg.get(msg.get('parent_title')),
-                    type=msg['type']
-                )
-                time.sleep(1)
-                new_id = confluence.get_page_id(target_space, msg['title'])
-                new_parent_msg[msg['title']] = new_id
-                # break
-            except Exception as e:
-                time.sleep(3)
-                confluence.create_page(
-                    space=target_space,
-                    title=msg['title'],
-                    body='wrong',
-                    type=msg['type']
-                )
-                print(e.args)
-
-
+            for i in range(10):
+                try:
+                    status = confluence.create_page(
+                        space=target_space,
+                        title=msg['title'],
+                        body=msg['body'],
+                        parent_id=new_parent_msg.get(msg.get('parent_title'))
+                        # type=msg['type']
+                    )
+                    time.sleep(1)
+                    new_id = confluence.get_page_id(target_space, msg['title'])
+                    new_parent_msg[msg['title']] = new_id
+                    break
+                except Exception as e:
+                    time.sleep(3)
+                    print(e.args)
 
 
 if __name__ == "__main__":
 
-    source_space = 'XXX'
-    target_space ='XXX'
-    URL ='XXX'
-    USERNAME = 'XXX'
-    PASSWD = 'XXX'
+    source_space = 'xxxxxxx'
+    URL = 'https://dwiki.daocloud.io/'
+    USERNAME = 'xxxxxxxxxxx'
+    PASSWD = 'xxxxxxxxxxxx'
 
-    a = ConfluenceService(URL, USERNAME, PASSWD, source_space, target_space)
-    # print(a.msg)
-    # create_body_other_website(URL, USERNAME, PASSWD, a.msg, a.source_space, target_space)
+    a = ConfluenceService(URL, USERNAME, PASSWD)
+    msg= a.get_body(GET_BODY_SPACE)
+    # a.create_body(msg, '~huifeng.tang')
+    # create_body_other_website(URL, USERNAME, PASSWD, msg, target_space, source_space) 
